c01f318ffd4b0d36ff1a558caf140fa0270b54ee
xyce3849 <70830516+xyce3849@users.noreply.github.com>
2026-08-30 02:28:44 +0800

    Add files via upload

---
+++ src/kernels/6.6.30-android15-8-gc6f5283046c6-ab12364222-4k/offsets.h
@@ -0,0 +1,20 @@
+OFFSETS_ENTRY(
+    "6.6.30-android15-8-gc6f5283046c6-ab12364222-4k",
+    STRUCT_OFFSETS_6_6,
+    .off_init_task = 0x205CAC0,
+    .off_init_cred = 0x206ED30,
+    .off_root_task_group = 0x224B500,
+    .off_selinux_enforcing = 0x2289AA8,
+    .off_selinux_blob_sizes = 0x1601648,
+    .off_security_hook_heads = 0x1600F10,
+    .off_slide_nfulnl_logger = 0x2051258,
+    .off_slide_boot_id = 0x22AAA88,
+    .off_slide_loggers_0_1 = 0x20511A8,
+    .pselect_waiter_shift = -2,
+)
+
+// STRUCT_PAGE_SIZE 0x40
+// STRUCT_PAGE_COMPOUND_HEAD 0x8
+// STRUCT_PAGE_TYPE 0x30
+// STRUCT_SLAB_CACHE 0x8
+// STRUCT_MM_STRUCT 0x4C0
